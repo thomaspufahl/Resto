91a805e29ff68d37bdc20674d0b3b944636a4e7f
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-11-02 05:00:32 -0300

    Add DataAccess

---
+++ RestoService/Database/DataAccess.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestoService.Database
+{
+    internal class DataAccess
+    {
+        private SqlConnection _Conex;
+        private SqlCommand _Cmd;
+
+        public SqlDataReader Reader { get; private set; }
+
+        public DataAccess()
+        {
+            _Conex = new SqlConnection();
+            _Cmd = new SqlCommand();
+
+            _Conex.ConnectionString = "server=localhost, 1433; database=Resto; user=sa; password=ThomasPufahl1525";
+        }
+
+        public void SetQuery(string query)
+        {
+            _Cmd.CommandType = System.Data.CommandType.Text;
+            _Cmd.CommandText = query;
+        }
+
+        public void Execute()
+        {
+            ExecuteSelectionQuery();
+        }
+        public void Execute(bool isAction)
+        {
+            if (isAction)
+            {
+                ExecuteActionQuery();
+            }
+            else
+            {
+                ExecuteSelectionQuery();
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (Reader != null && !Reader.IsClosed) Reader.Close();
+
+            _Conex.Close();
+        }
+
+        private void ExecuteSelectionQuery()
+        {
+            _Cmd.Connection = _Conex;
+
+            try
+            {
+                _Conex.Open();
+                Reader = _Cmd.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                CloseConnection();
+                throw new Exception("Error ocurring at database conex", ex);
+            }
+        }
+
+        private void ExecuteActionQuery()
+        {
+            _Cmd.Connection = _Conex;
+
+            try
+            {
+                _Conex.Open();
+                _Cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                CloseConnection();
+                throw new Exception("Error ocurring at database conex", ex);
+            }
+        }
+    }
+}
