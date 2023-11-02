722446a09ccd52ae6ae3d63e2d63a4403bf76f28
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-11-02 05:13:34 -0300

    Add RestoManager

---
+++ RestoService/RestoManager.cs
@@ -0,0 +1,20 @@
+﻿using RestoShared;
+using RestoShared.ITable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestoService
+{
+    public class RestoManager : IRestoManager
+    {
+        public IRole Role { get; }
+
+        public RestoManager()
+        {
+            
+        }
+    }
+}
