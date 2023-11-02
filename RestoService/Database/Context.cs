25e480f07661c259f171b9033d5cde60c03eecd4
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-11-02 04:59:18 -0300

    Create Context

---
+++ RestoService/Database/Context.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestoService.Database
+{
+    internal class Context
+    {
+        private static readonly Context _Instance = new Context();
+        
+        private Context()
+        {
+        }
+
+        public static Context Instance
+        {
+            get
+            {
+                return _Instance;
+            }
+        }
+    }
+}
