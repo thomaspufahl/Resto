9da3aeaa8b9ab2e2e6460560c71b50eba27276d9
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-11-02 04:54:39 -0300

    Add IRole

---
+++ RestoShared/ITable/IRole.cs
@@ -0,0 +1,17 @@
+﻿using RestoShared.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestoShared.ITable
+{
+    public interface IRole
+    {
+        int RoleId { get; }
+        string Name { get; }
+        string Description { get; }
+        List<RoleDTO> GetAll();
+    }
+}
