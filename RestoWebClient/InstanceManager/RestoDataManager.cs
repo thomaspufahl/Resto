804046f5fc017f54b21e2ad96b77e62a29b01136
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-11-02 06:30:35 -0300

    Rename RestoManagerInstance to RestoDataManager

---
+++ RestoWebClient/InstanceManager/RestoDataManager.cs
@@ -0,0 +1,25 @@
+﻿using RestoService;
+using RestoShared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestoWebClient.InstanceManager
+{
+    public class RestoDataManager
+    {
+        private static IRestoManager _Instance;
+        public static IRestoManager Data
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    _Instance = new RestoManager();
+                }
+                return _Instance;
+            }
+        }
+    }
+}
