ad0414b55b743a342bcc832ef7283a23d97f46b4
coutnico <nicoo_rodrii@hotmail.com>
2023-11-03 05:58:55 -0300

    Create IEmployee

---
+++ RestoShared/ITable/IEmployee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestoShared.DTO;
+
+
+namespace RestoShared.ITable
+{
+
+    public interface IEmployee
+    {
+        int EmployeeId { get; }
+
+        string EmployeeNumber { get; }
+        string FirstName { get; }
+        string LastName { get; }
+        int RoleId { get; }
+
+        bool IsActive { get; }
+
+        void Initialize(EmployeeDTO employeeDTO);
+
+        int Add();
+
+        void Update();
+
+        EmployeeDTO GetById();
+
+        int Activate();
+
+        int Desactivate();
+        
+        void Delete();
+
+        List<EmployeeDTO> GetAll();
+
+    }
+}
