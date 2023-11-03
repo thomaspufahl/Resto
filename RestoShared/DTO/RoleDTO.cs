95330f0eab58835a470fe3840a8b6c0bd2fbcd53
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-11-03 06:34:18 -0300

    Add prop IsActive

--- RestoShared/DTO/RoleDTO.cs
+++ RestoShared/DTO/RoleDTO.cs
@@ -11,5 +11,6 @@ namespace RestoShared.DTO
         public int RoleId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public bool IsActive { get; set; }
     }
 }
