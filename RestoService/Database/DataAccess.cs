d0202f05ad8eb7ebd3c97f941bab8bdeb8c626e7
coutnico <nicoo_rodrii@hotmail.com>
2023-11-03 00:24:52 -0300

    Add Personal ConnectionString in DataAccess

--- RestoService/Database/DataAccess.cs
+++ RestoService/Database/DataAccess.cs
@@ -9,7 +9,8 @@ namespace RestoService.Database
 {
     internal class DataAccess
     {
-        private readonly string _ConnectionString = "server=localhost, 1433; database=Resto; user=sa; password=ThomasPufahl1525";
+        private readonly string _ConnectionStringP = "server=localhost, 1433; database=Resto; user=sa; password=ThomasPufahl1525";
+        private readonly string _ConnectionStringN = "server=localhost, 1433; database=Resto; user=sa; password=Verdeverde8";
         private SqlConnection _Conex;
         private SqlCommand _Cmd;
 
@@ -20,7 +21,7 @@ namespace RestoService.Database
             _Conex = new SqlConnection();
             _Cmd = new SqlCommand();
 
-            _Conex.ConnectionString = _ConnectionString;
+            _Conex.ConnectionString = _ConnectionStringN;
         }
 
         public void SetQuery(string query)
