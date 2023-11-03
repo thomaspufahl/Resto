b386f5bf19b36467ef2cdb9a1c512aef6d827def
coutnico <nicoo_rodrii@hotmail.com>
2023-11-03 06:01:22 -0300

    Change in DataAccess

--- RestoService/Database/DataAccess.cs
+++ RestoService/Database/DataAccess.cs
@@ -9,8 +9,7 @@ namespace RestoService.Database
 {
     internal class DataAccess
     {
-        private readonly string _ConnectionStringP = "server=localhost, 1433; database=Resto; user=sa; password=ThomasPufahl1525";
-        private readonly string _ConnectionStringN = "server=localhost, 1433; database=Resto; user=sa; password=Verdeverde8";
+        private readonly string _ConnectionString = "server=localhost, 1433; database=Resto; user=sa; password=Resto123";
         private SqlConnection _Conex;
         private SqlCommand _Cmd;
 
@@ -21,7 +20,7 @@ namespace RestoService.Database
             _Conex = new SqlConnection();
             _Cmd = new SqlCommand();
 
-            _Conex.ConnectionString = _ConnectionStringN;
+            _Conex.ConnectionString = _ConnectionString;
         }
 
         public void SetQuery(string query)
