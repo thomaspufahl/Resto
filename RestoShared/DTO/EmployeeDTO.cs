cf40f7ac63df8bc3e44bce674ae32a67f91fdf47
coutnico <nicoo_rodrii@hotmail.com>
2023-11-03 05:58:37 -0300

    Create EmployeeDTO

---
+++ RestoShared/DTO/EmployeeDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestoShared.DTO
+{
+    public class EmployeeDTO
+    {
+        public int EmployeeId{ get; set; }
+
+        public string EmployeeNumber { get; set; }
+
+        public string FirstName{ get; set; }
+
+        public string LastName { get; set; }
+
+        public int RoleId { get; set; }
+
+        public bool IsActive { get; set; }
+
+    }
+}
