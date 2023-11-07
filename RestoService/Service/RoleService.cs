2819dd4f418ce54e4d1831c66a11930bdb22281f
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-11-07 15:49:13 -0300

    Add RoleDTO fields to RoleService

--- RestoService/Service/RoleService.cs
+++ RestoService/Service/RoleService.cs
@@ -12,12 +12,32 @@ namespace RestoService.Service
     internal class RoleService : IRole
     {
         private readonly DataAccess db;
+        private bool _IsInitialized = false;
+
+        public int RoleId { get; private set; }
+        public string RoleName { get; private set; }
+        public string RoleDescription { get; private set; }
+        public bool IsActive { get; private set; }
 
         public RoleService()
         {
             db = new DataAccess();
         }
 
+        private void CheckInitialized()
+        {
+            if (!_IsInitialized)
+            {
+                throw new InvalidOperationException("Role is not initialize");
+            }
+        }
+        public void Initialize(RoleDTO roleDTO)
+        {
+            RoleId = roleDTO.RoleId;
+            RoleName = roleDTO.RoleName;
+            RoleDescription = roleDTO.RoleDescription;
+            IsActive = roleDTO.IsActive;
+        }
         public List<RoleDTO> GetAll()
         {
             List<RoleDTO> roleList = new List<RoleDTO>();
