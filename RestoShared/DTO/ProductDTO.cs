2c3281eb37b39af9d8408f02a76e6fdc38e077a5
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-11-08 11:49:34 -0300

    Create ProductDTO

---
+++ RestoShared/DTO/ProductDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestoShared.DTO
+{
+    public class ProductDTO
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public int ProductCategoryId { get; set; }
+        public int Stock { get; set; }
+        public int MinStockLevel { get; set; }
+        public decimal UnitPrice { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
