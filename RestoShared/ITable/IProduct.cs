70dcebf3bd7fef0bb13a2b64c567502215eb1060
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-11-08 11:51:29 -0300

    Create IProduct

---
+++ RestoShared/ITable/IProduct.cs
@@ -0,0 +1,30 @@
+﻿using RestoShared.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestoShared.ITable
+{
+    public interface IProduct
+    {
+        int ProductId { get; }
+        string ProductName { get; }
+        string ProductDescription { get; }
+        int ProductCategoryId { get; }
+        int Stock { get; }
+        int MinStockLevel { get; }
+        decimal UnitPrice { get; }
+        bool IsActive { get; }
+
+        void Initialize(ProductDTO productDTO);
+        ServiceResponse<int> Add();
+        ServiceResponse<List<ProductDTO>> GetAll();
+        ServiceResponse<ProductDTO> GetById(int productId);
+        ServiceResponse<int> Update();
+        ServiceResponse<int> Delete();
+        ServiceResponse<int> Activate();
+        ServiceResponse<int> Deactivate();
+    }
+}
