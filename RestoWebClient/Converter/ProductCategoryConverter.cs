9917bf44c12712268a73788389b908ebb380a20c
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-11-09 13:50:06 -0300

    Create ProductCategoryConverter

---
+++ RestoWebClient/Converter/ProductCategoryConverter.cs
@@ -0,0 +1,41 @@
+﻿using RestoShared.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestoWebClient
+{
+    public class ProductCategoryConverter
+    {
+        public static string Convert(int productCategoryId)
+        {
+            List<ProductCategoryDTO> list = SessionManager.ProductCategoryList;
+
+            foreach (ProductCategoryDTO category in list)
+            {
+                if (category.ProductCategoryId == productCategoryId)
+                {
+                    return category.ProductCategoryName;
+                }
+            }
+
+            return "Unknown";
+        }
+
+        public static int Convert(string productCategoryName)
+        {
+            List<ProductCategoryDTO> list = SessionManager.ProductCategoryList;
+
+            foreach (ProductCategoryDTO category in list)
+            {
+                if (category.ProductCategoryName == productCategoryName)
+                {
+                    return category.ProductCategoryId;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
