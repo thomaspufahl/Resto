77bb445cc056c8182437e784be7c90525e387e03
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-11-08 23:00:16 -0300

    Add IRestoSecurityManager

---
+++ RestoShared/IRestoSecurityManager.cs
@@ -0,0 +1,26 @@
+﻿using RestoShared.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestoShared
+{
+    public enum AccessLevel
+    {
+        NOT_LOGGED = 0,
+        LOGGED = 1,
+        MANAGER = 2,
+    }
+    public interface IRestoSecurityManager
+    {
+        EmployeeDTO LoggedUser { get; }
+        bool IsLogged { get; }
+        bool IsLoggedAsManager { get; }
+
+        ServiceResponse<bool> Login(string employeeNumber);
+        ServiceResponse<bool> Logout();
+        bool IsAuthorized(AccessLevel accessLevel);
+    }
+}
