27f7681f166a77a18f65da6c7e38ff11038ea5be
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-11-09 13:30:19 -0300

    Create RoleConverter

---
+++ RestoWebClient/Converter/RoleIdToRoleNameConverter.cs
@@ -0,0 +1,41 @@
+﻿using RestoShared.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestoWebClient
+{
+    public class RoleIdToRoleNameConverter 
+    {
+        public static string Convert(int roleId)
+        {
+            List<RoleDTO> list = SessionManager.RoleList;
+
+            foreach (RoleDTO role in list)
+            {
+                if (role.RoleId == roleId)
+                {
+                    return role.RoleName;
+                }
+            }
+
+            return "Unknown";
+        }
+
+        public static int Convert(string roleName)
+        {
+            List<RoleDTO> list = SessionManager.RoleList;
+
+            foreach (RoleDTO role in list)
+            {
+                if (role.RoleName == roleName)
+                {
+                    return role.RoleId;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
