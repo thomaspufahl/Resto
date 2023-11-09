d26cce8939368e594794ae5bb677412fa4bff579
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-11-09 13:29:54 -0300

    Implement Data in session

--- RestoWebClient/SessionManager.cs
+++ RestoWebClient/SessionManager.cs
@@ -13,14 +13,15 @@ namespace RestoWebClient
 {
     public static class SessionManager
     {
-        private static HttpSessionState Session 
-        { 
+        private static HttpSessionState Session
+        {
             get
             {
                 return HttpContext.Current.Session;
             }
         }
 
+        // SECURITY
         public static EmployeeDTO LoggedUser
         {
             get
@@ -72,6 +73,103 @@ namespace RestoWebClient
             Session["IsLoggedAsManager"] = false;
             Session["IsLogged"] = false;
             Session["LoggedAccessLevel"] = AccessLevel.NOT_LOGGED;
+
+            return true;
+        }
+
+        // DATA
+        // Role
+        public static List<RoleDTO> RoleList
+        {
+            get
+            {
+                if (Session["RoleList"] == null)
+                {
+                    if (!LoadRoleList()) return null;
+                }
+
+                return (List<RoleDTO>)Session["RoleList"];
+            }
+        }
+        public static bool LoadRoleList()
+        {
+            var response = RestoManager.Data.Role.GetAll();
+
+            if (!response.IsSuccess) return false;
+
+            Session["RoleList"] = response.Data;
+
+            return true;
+        }
+
+        // Employee
+        public static List<EmployeeDTO> EmployeeList
+        {
+            get
+            {
+                if (Session["EmployeeList"] == null)
+                {
+                    if (!LoadEmployeeList()) return null;
+                }
+
+                return (List<EmployeeDTO>)Session["EmployeeList"];                
+            }
+        }
+        public static bool LoadEmployeeList()
+        {
+            var response = RestoManager.Data.Employee.GetAll();
+
+            if (!response.IsSuccess) return false;
+
+            Session["EmployeeList"] = response.Data;
+
+            return true;
+        }
+
+        // ProductCategory
+        public static List<ProductCategoryDTO> ProductCategoryList
+        {
+            get
+            {
+                if (Session["ProductCategoryList"] == null)
+                {
+                    if (!LoadProductCategoryList()) return null;
+                }
+
+                return (List<ProductCategoryDTO>)Session["ProductCategoryList"];
+            }
+        }
+        public static bool LoadProductCategoryList()
+        {
+            var response = RestoManager.Data.ProductCategory.GetAll();
+
+            if (!response.IsSuccess) return false;
+
+            Session["ProductCategoryList"] = response.Data;
+
+            return true;
+        }
+
+        // Product
+        public static List<ProductDTO> ProductList
+        {
+            get
+            {
+                if (Session["ProductList"] == null)
+                {
+                    if (!LoadProductList()) return null;
+                }
+
+                return (List<ProductDTO>)Session["ProductList"];
+            }
+        }
+        public static bool LoadProductList()
+        {
+            var response = RestoManager.Data.Product.GetAll();
+
+            if (!response.IsSuccess) return false;
+
+            Session["ProductList"] = response.Data;
 
             return true;
         }
