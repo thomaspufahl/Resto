84037f85ff314a0cdff8829a96504b0d4f9eb280
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-11-08 23:01:21 -0300

    Add Router in WebClient

---
+++ RestoWebClient/Router.cs
@@ -0,0 +1,101 @@
+﻿using RestoService;
+using RestoShared;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+namespace RestoWebClient
+{
+    public enum RouteName
+        {
+            HOME,
+            DEFAULT,
+            LOGIN,
+            NOT_FOUND,
+
+            REGISTER,
+
+            ORDERS,
+            ORDERITEMFORM,
+
+            EMPLOYEES,
+            EMPLOYEEFORM,
+
+            PRODUCTS,
+            PRODUCTFORM,
+
+            REPORTS,
+        }
+
+    public class Router
+    {
+        private readonly Dictionary<RouteName, AccessLevel> RouteAuth = new Dictionary<RouteName, AccessLevel>()
+        {
+            { RouteName.HOME, AccessLevel.NOT_LOGGED },
+            { RouteName.DEFAULT, AccessLevel.NOT_LOGGED },
+            { RouteName.LOGIN, AccessLevel.NOT_LOGGED },
+            { RouteName.NOT_FOUND, AccessLevel.NOT_LOGGED },
+            { RouteName.REGISTER, AccessLevel.MANAGER },
+            { RouteName.ORDERS, AccessLevel.LOGGED },
+            { RouteName.ORDERITEMFORM, AccessLevel.LOGGED },
+            { RouteName.EMPLOYEES, AccessLevel.MANAGER },
+            { RouteName.EMPLOYEEFORM, AccessLevel.MANAGER },
+            { RouteName.PRODUCTS, AccessLevel.MANAGER },
+            { RouteName.PRODUCTFORM, AccessLevel.MANAGER },
+            { RouteName.REPORTS, AccessLevel.MANAGER },
+        };
+
+        public readonly Dictionary<RouteName, string> RouteUrl = new Dictionary<RouteName, string>()
+        {
+            { RouteName.HOME, "/" },
+            { RouteName.DEFAULT, "Default.aspx"},
+            { RouteName.LOGIN, "LoginForm.aspx"},
+            { RouteName.NOT_FOUND, "404.aspx"},
+            { RouteName.REGISTER, "RegisterForm.aspx"},
+            { RouteName.ORDERS, "Orders.aspx" },
+            { RouteName.ORDERITEMFORM, "OrderItemForm.aspx" },
+            { RouteName.EMPLOYEES, "Employees.aspx" },
+            { RouteName.EMPLOYEEFORM, "EmployeeForm.aspx" },
+            { RouteName.PRODUCTS, "Products.aspx" },
+            { RouteName.PRODUCTFORM, "ProductForm.aspx" },
+            { RouteName.REPORTS, "Reports.aspx" },
+        };
+
+        public void AuthorizeNavigation(string path)
+        {   
+            // check if path exists in RouteUrl
+            foreach (KeyValuePair<RouteName, string> route in RouteUrl)
+            {
+                if (route.Value == path)
+                {
+                    // check if user is authorized to access this route
+                    if (RestoManager.Security.IsAuthorized(RouteAuth[route.Key]))
+                    {
+                        RedirectTo(RouteName.LOGIN);
+                    }
+                }
+            }
+        }
+        public void RedirectTo(RouteName route)
+        {
+            HttpContext.Current.Response.Redirect(RouteUrl[route], false);
+        }
+
+        // Test if this works
+        public void RedirectTo(RouteName route, string query)
+        {
+            HttpContext.Current.Response.Redirect(RouteUrl[route] + query, false);
+        }
+        public void RedirectTo(RouteName route, string query, string queryValue)
+        {
+            HttpContext.Current.Response.Redirect(RouteUrl[route] + "?" + query + "=" + queryValue, false);
+        }
+        public void RedirectTo(RouteName route, string query, int queryValue)
+        {
+            HttpContext.Current.Response.Redirect(RouteUrl[route] + "?" + query + "=" + queryValue, false);
+        }
+    }
+}
