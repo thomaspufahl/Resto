90e04e60515cb23fac7cf299eb2de5c745aded6d
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-11-08 23:02:41 -0300

    Add session manager login demo in WebClient

--- RestoWebClient/Resto.Master.cs
+++ RestoWebClient/Resto.Master.cs
@@ -10,8 +10,18 @@ namespace RestoWebClient
     public partial class Resto : System.Web.UI.MasterPage
     {
         private readonly Router Router = new Router();
+        SessionManager sessionManager;
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                sessionManager = new SessionManager(Page);
+
+                if (!sessionManager.IsSessionActive)
+                {
+                    sessionManager.Login("E66666666");
+                }
+            }
 
             Router.AuthorizeNavigation(HttpContext.Current.Request.Url.AbsolutePath.Substring(1));
         }
