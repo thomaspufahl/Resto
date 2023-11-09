a1cb976366c7850734d792eac8251ba3025a5e82
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-11-09 13:49:41 -0300

    Create UnitPriceConverter

---
+++ RestoWebClient/Converter/UnitPriceConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestoWebClient
+{
+    public class UnitPriceConverter
+    {
+        public static string Convert(decimal unitPrice)
+        {
+            return unitPrice.ToString("#.###");
+        }
+    }
+}
