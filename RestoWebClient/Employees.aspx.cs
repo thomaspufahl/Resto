a72990518d2e165c199036cde46b8559ac487d00
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-11-09 13:31:00 -0300

    Add employeeList demo

--- RestoWebClient/Employees.aspx.cs
+++ RestoWebClient/Employees.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestoShared.DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,11 @@ namespace RestoWebClient
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                EmployeeList.DataSource = SessionManager.EmployeeList;
+                EmployeeList.DataBind();
+            }
         }
     }
 }
--- RestoWebClient/Employees.aspx.designer.cs
+++ RestoWebClient/Employees.aspx.designer.cs
@@ -13,5 +13,14 @@ namespace RestoWebClient
 
     public partial class Employees
     {
+
+        /// <summary>
+        /// EmployeeList control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater EmployeeList;
     }
 }
