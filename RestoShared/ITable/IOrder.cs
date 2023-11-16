eceaff9c4d2f9195100580bdb0cb2dc3889d02f7
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-11-15 22:55:23 -0300

    Implement Order

---
+++ RestoShared/ITable/IOrder.cs
@@ -0,0 +1,29 @@
+﻿using RestoShared.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestoShared.ITable
+{
+    public interface IOrder
+    {
+        long OrderNumber { get; }
+        int EmployeeId { get; }
+        byte TableNumber { get; }
+        DateTime OrderDate { get; }
+        int OrderStatusId { get; }
+        DateTime UpdatedAt { get; }
+        bool IsActive { get; }
+
+        void Initialize(OrderDTO orderDTO);
+        ServiceResponse<int> Add();
+        ServiceResponse<List<OrderDTO>> GetAll();
+        ServiceResponse<OrderDTO> GetByOrderNumber(long orderNumber);
+        ServiceResponse<int> Update();
+        ServiceResponse<int> Delete();
+        ServiceResponse<int> Activate();
+        ServiceResponse<int> Deactivate();
+    }
+}
