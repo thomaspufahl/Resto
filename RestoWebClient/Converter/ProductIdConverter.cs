63fdbe93e5feab7a98a5ffc91d3c235458fc1a40
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-11-23 14:02:42 -0300

    Create ProductIdConverter

---
+++ RestoWebClient/Converter/ProductIdConverter.cs
@@ -0,0 +1,41 @@
+﻿using RestoShared.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestoWebClient
+{
+    public class ProductIdConverter
+    {
+        public static string Convert(int productId)
+        {
+            List<ProductDTO> list = SessionManager.ProductList;
+
+            foreach (ProductDTO product in list)
+            {
+                if (product.ProductId == productId)
+                {
+                    return product.ProductName;
+                }
+            }
+
+            return "Unknown";
+        }
+
+        public static int Convert(string productName)
+        {
+            List<ProductDTO> list = SessionManager.ProductList;
+
+            foreach (ProductDTO product in list)
+            {
+                if (product.ProductName == productName)
+                {
+                    return product.ProductId;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
