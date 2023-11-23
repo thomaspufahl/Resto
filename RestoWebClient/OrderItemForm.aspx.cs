2180ff8b36a23fbebec1e21c447074a196c410d6
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-11-23 14:25:11 -0300

    Catch OrderNumber from request

--- RestoWebClient/OrderItemForm.aspx.cs
+++ RestoWebClient/OrderItemForm.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestoShared.DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,11 @@ namespace RestoWebClient
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (Request.QueryString["orderNumber"] != null)
+            {
+                long orderNumber = Convert.ToInt64(Request.QueryString["orderNumber"]);
+                OrderDTO ord = SessionManager.OrderByOrderNumber(orderNumber);
+            }
         }
     }
 }
