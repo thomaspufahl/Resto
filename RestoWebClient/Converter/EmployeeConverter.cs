2ccc06913079235fc391ce133bc84d17d350063b
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-11-28 19:36:32 -0300

    Create Employee converter

---
+++ RestoWebClient/Converter/EmployeeConverter.cs
@@ -0,0 +1,29 @@
+﻿using RestoShared.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestoWebClient
+{
+    public class EmployeeConverter
+    {
+        public static string Convert(int employeeId)
+        {
+            EmployeeDTO emp = SessionManager.EmployeeById(employeeId);
+
+            if (emp == null) return "Unknown employee";
+
+            return emp.FirstName + " " + emp.LastName;
+        }
+
+        public static string Number(int employeeId)
+        {
+            EmployeeDTO emp = SessionManager.EmployeeById(employeeId);
+
+            if (emp == null) return "Unknown employee";
+
+            return emp.EmployeeNumber;
+        }
+    }
+}
